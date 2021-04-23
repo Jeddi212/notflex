e19a6c710ca74a0c18139df03e887a3b50a363da
Jeddi212 <jedediahf51@gmail.com>
2021-04-23 21:44:13 +0700

    Silent Log Level

--- controllers/db_handler.go
+++ controllers/db_handler.go
@@ -14,9 +14,9 @@ func Connect() *gorm.DB {
 	newLogger := logger.New(
 		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
 		logger.Config{
-			SlowThreshold: time.Second, // Slow SQL threshold
+			SlowThreshold: time.Second,   // Slow SQL threshold
 			LogLevel:      logger.Silent, // Log level  // Ignore ErrRecordNotFound error for logger
-			Colorful:      false,       // Disable color
+			Colorful:      false,         // Disable color
 		},
 	)
 	dsn := "root@tcp(localhost:3306)/db_notflex?charset=utf8mb4&parseTime=true&loc=Asia%2FJakarta"
