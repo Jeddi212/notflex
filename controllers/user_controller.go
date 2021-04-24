4f14bfcfb92c07c81e182ceb1d9e71d70f12fc5d
Jeddi212 <jedediahf51@gmail.com>
2021-04-24 13:44:00 +0700

    Pindahin FK ke Credit

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -88,38 +88,48 @@ func InsertMember(w http.ResponseWriter, r *http.Request) {
 	birthDate := r.Form.Get("birthdate")
 	gender := r.Form.Get("gender")
 	nationality := r.Form.Get("nationality")
-	ccNumber := r.Form.Get("creditCardNumber")
-	exp := r.Form.Get("expire")
-	cvc := r.Form.Get("cvc")
+	//ccNumber := r.Form.Get("creditCardNumber")
+	//exp := r.Form.Get("expire")
+	//cvc := r.Form.Get("cvc")
 
 	var userCek models.User
 	var response models.UserResponse
 	err = db.Where("email = ?", email).First(&userCek).Error
 	if errors.Is(err, gorm.ErrRecordNotFound) == true {
-		cc := models.Credit{CardNumber: ccNumber, Exp: exp, Cvc: cvc}
-		user := models.User{Email: email, Password: password, Name: name, BirthDate: birthDate, Gender: gender, Nationality: nationality, Status: "active", Level: 1, CreditID: ccNumber}
-
-		if ccNumber != "" {
-			var ccCek models.Credit
-			err = db.Where("card_number = ?", ccNumber).First(&ccCek).Error
-			if errors.Is(err, gorm.ErrRecordNotFound) == true {
-				db.Create(&cc)
-				if err := db.Create(&user).Error; err == nil {
-					response.Status = 200
-					response.Message = "Registration success"
-				} else {
-					db.Where("card_number = ?", ccNumber).Delete(&cc)
-					response.Status = 400
-					response.Message = "Registration failed"
-				}
-			} else {
-				response.Status = 400
-				response.Message = "Credit card number has been registrered"
-			}
-		} else {
-			response.Status = 400
-			response.Message = "Registration credit card failed"
+		//cc := models.Credit{CardNumber: ccNumber, Exp: exp, Cvc: cvc}
+		user := models.User{
+			Email:       email,
+			Password:    password,
+			Name:        name,
+			BirthDate:   birthDate,
+			Gender:      gender,
+			Nationality: nationality,
+			Status:      "active",
+			Level:       1,
+			/*CreditID: ccNumber,*/
 		}
+
+		//if ccNumber != "" {
+		//	var ccCek models.Credit
+		//	err = db.Where("card_number = ?", ccNumber).First(&ccCek).Error
+		//	if errors.Is(err, gorm.ErrRecordNotFound) == true {
+		//		//db.Create(&cc)
+		if err := db.Create(&user).Error; err == nil {
+			response.Status = 200
+			response.Message = "Registration success"
+		} else {
+			//db.Where("card_number = ?", ccNumber).Delete(&cc)
+			response.Status = 400
+			response.Message = "Registration failed" + err.Error()
+		}
+		//	} else {
+		//		response.Status = 400
+		//		response.Message = "Credit card number has been registrered"
+		//	}
+		//} else {
+		//	response.Status = 400
+		//	response.Message = "Registration credit card failed"
+		//}
 	} else {
 		response.Status = 400
 		response.Message = "Email or has been taken"
